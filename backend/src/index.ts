cda20205b42a3f50dbc56344d6e790e6cf01bdea
Pratiksha Gunjal <gunjalpratiksha2003@gmail.com>
2024-11-23 21:06:19 +0530

    update the code 3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
 import postRoutes from './routes/destination';
 import userRoutes from './routes/users';
+import User from './models/User';
 
 dotenv.config();
 const app = express();
@@ -24,6 +25,17 @@ app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => { 
   res.send('Backend server is running');
+});
+
+app.get('/users', async(req, res) => {
+   try {
+        const users = await User.find();
+        console.log( "user=========",users);
+        res.status(200).json(users);
+        // res.status(200).json({ message: 'Get all users' });
+    } catch (err) {
+        res.status(500).json({ message: 'server error' });
+    }
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
