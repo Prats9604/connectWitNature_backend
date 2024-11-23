30a5ecba55dbc7e9385a573d3cc1ec1f3c249361
Pratiksha Gunjal <gunjalpratiksha2003@gmail.com>
2024-11-23 20:45:25 +0530

    update the code 1

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -6,8 +6,9 @@ const router= express.Router();
 //Get All Users
 router.get('/', async (req, res) => {
     try {
-        const users: IUser[] = await User.find();
-        res.status(200).json(users);
+        // const users: IUser[] = await User.find();
+        // res.status(200).json(users);
+        res.status(200).json({ message: 'Get all users' });
     } catch (err) {
         res.status(500).json({ message: 'server error' });
     }
